cf52a8097fbb69b680b39e59850e9471e4daa674
Vitorhrds <110406986+Vitorhrds2@users.noreply.github.com>
2023-05-07 22:49:30 -0300

    Exercício 2 realizado

--- exercicio2.py
+++ exercicio2.py
@@ -5,7 +5,24 @@
 
 
 # Crie a função que será avaliada no exercício aqui
+def is_anagram(string1, string2):
+    # Remove espaços em branco e converte as strings em minúsculas
+    string1 = string1.replace(' ', '').lower()
+    string2 = string2.replace(' ', '').lower()
 
+    # Verifica se os comprimentos das strings são iguais
+    if len(string1) != len(string2):
+        return False
+
+    # Converte as strings em listas e as ordena
+    lista1 = sorted(list(string1))
+    lista2 = sorted(list(string2))
+
+    # Compara as listas ordenadas para verificar se são iguais
+    if lista1 == lista2:
+        return True
+    else:
+        return False
 
 
 
